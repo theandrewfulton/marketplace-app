e897f85272efcc692edd36444eea54758fbf5316
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-03-09 16:56:30 +1100

    add devise gem to project

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root to: 'welcome#index'
 end
