37c198f8be0272e1fa7d104ce09bc6f1853370d2
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-03-25 11:20:14 +1100

    continued stripe integration, broken params

--- src/app/controllers/payments_controller.rb
+++ src/app/controllers/payments_controller.rb
@@ -1,4 +1,26 @@
 class PaymentsController < ApplicationController
+    skip_before_action :verify_authenticity_token
     def success
+    end
+
+    def webhook
+        payment_id = params[:data][:object][:payment_intent]
+        payment = Stripe::PaymentIntent.retrieve(payment_id)
+        listing_id = payment.metadata.listing_id
+        user_id = payment.metadata.user_id
+        puts "listing_id:"
+        p listing_id
+        puts "user_id:"
+        p user_id
+
+        status 200
+        # listing = Listing.find(listing_id.to_i)
+        # listing.sold = true
+        # listing.save
+
+        # # # update available quantity
+        # # listing - Listing.find(listing_id.to_i)
+        # # # listing.quantity_available -= params with quantity in it
+        # # listing.save
     end
 end
